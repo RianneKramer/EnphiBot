cf332297285d0d9b8e3a137188f4706515b3f029
Rianne Kramer - Enphia <167865270+RianneKramer@users.noreply.github.com>
2025-12-08 14:55:52 +0100

    Added closeTicket, needs to be developed further

---
+++ src/03modals/moderation/closeTicket.js
@@ -0,0 +1,10 @@
+const { ChannelType, PermissionsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { modRole } = require('../../../config.json')
+
+module.exports = {
+    name: 'closeTicket',
+
+    callback: async (client, interaction) => {
+        // add the response logic here
+    }
+}
